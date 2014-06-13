eca3e605d3838d6ef2f8c50c1ed8a17d6b0b748d
Jeru <jeru.liu@gmail.com>
2014-06-13 14:17:15 +0800

    Created order table

---
+++ application/_install/10-create-table-order.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `customer_id` varchar(20) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `is_diy` int(1) NOT NULL,
+  `total_amount` double(10,2) NOT NULL,
+  `address_id` varchar(20) NOT NULL,
+  `created_time` datetime NOT NULL,
+  `updated_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
