2620cb7b4ad5a45f958afb976d49d6fa058e5d6a
Jeru <jeru.liu@gmail.com>
2014-06-16 00:18:27 +0800

    Refined Credential UI

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -46,7 +46,7 @@ if (!session_id()) session_start();
 </head>
 
 <body class="" style="">
-<!--<script async="" src="/orchard7/public/js/analytics.js"></script>-->
+<!--<script async="" src="/orchard7/public/js/analytics.js"></script>
 <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -55,7 +55,7 @@ if (!session_id()) session_start();
 
     ga('create', 'UA-44426473-2', 'redteamux.com');
     ga('send', 'pageview');
-</script>
+</script>-->
 
 <div id="headerbar">
     <div class="container">
@@ -160,6 +160,151 @@ if (!session_id()) session_start();
             </ul>
         </li>
 
+        <li class="dropdown">
+            <a href="#" class="hasnotifications dropdown-toggle" data-toggle='dropdown'><i class="fa fa-envelope"></i><span class="badge">1</span></a>
+            <ul class="dropdown-menu messages arrow">
+                <li class="dd-header">
+                    <span>You have 1 new message(s)</span>
+                    <span><a href="#">Mark all Read</a></span>
+                </li>
+                <div class="scrollthis">
+                    <li><a href="#" class="active">
+                            <span class="time">6 mins</span>
+                            <img src="assets/demo/avatar/doyle.png" alt="avatar" />
+                            <div><span class="name">Alan Doyle</span><span class="msg">Please mail me the files by tonight.</span></div>
+                        </a></li>
+                    <li><a href="#">
+                            <span class="time">12 mins</span>
+                            <img src="assets/demo/avatar/paton.png" alt="avatar" />
+                            <div><span class="name">Polly Paton</span><span class="msg">Uploaded all the files to server. Take a look.</span></div>
+                        </a></li>
+                    <li><a href="#">
+                            <span class="time">9 hrs</span>
+                            <img src="assets/demo/avatar/corbett.png" alt="avatar" />
+                            <div><span class="name">Simon Corbett</span><span class="msg">I am signing off for today.</span></div>
+                        </a></li>
+                    <li><a href="#">
+                            <span class="time">2 days</span>
+                            <img src="assets/demo/avatar/tennant.png" alt="avatar" />
+                            <div><span class="name">David Tennant</span><span class="msg">How are you doing?</span></div>
+                        </a></li>
+                    <li><a href="#">
+                            <span class="time">6 mins</span>
+                            <img src="assets/demo/avatar/doyle.png" alt="avatar" />
+                            <div><span class="name">Alan Doyle</span><span class="msg">Please mail me the files by tonight.</span></div>
+                        </a></li>
+                    <li><a href="#">
+                            <span class="time">12 mins</span>
+                            <img src="assets/demo/avatar/paton.png" alt="avatar" />
+                            <div><span class="name">Polly Paton</span><span class="msg">Uploaded all the files to server. Take a look.</span></div>
+                        </a></li>
+                </div>
+                <li class="dd-footer"><a href="#">View All Messages</a></li>
+            </ul>
+        </li>
+
+        <li class="dropdown">
+            <a href="#" class="hasnotifications dropdown-toggle" data-toggle='dropdown'><i class="fa fa-bell"></i><span class="badge">3</span></a>
+            <ul class="dropdown-menu notifications arrow">
+                <li class="dd-header">
+                    <span>You have 3 new notification(s)</span>
+                    <span><a href="#">Mark all Seen</a></span>
+                </li>
+                <div class="scrollthis">
+                    <li>
+                        <a href="#" class="notification-user active">
+                            <span class="time">4 mins</span>
+                            <i class="fa fa-user"></i>
+                            <span class="msg">New user Registered. </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-danger active">
+                            <span class="time">20 mins</span>
+                            <i class="fa fa-bolt"></i>
+                            <span class="msg">CPU at 92% on server#3! </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-success active">
+                            <span class="time">1 hr</span>
+                            <i class="fa fa-check"></i>
+                            <span class="msg">Server#1 is live. </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-warning">
+                            <span class="time">2 hrs</span>
+                            <i class="fa fa-exclamation-triangle"></i>
+                            <span class="msg">Database overloaded. </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-order">
+                            <span class="time">10 hrs</span>
+                            <i class="fa fa-shopping-cart"></i>
+                            <span class="msg">New order received. </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-failure">
+                            <span class="time">12 hrs</span>
+                            <i class="fa fa-times-circle"></i>
+                            <span class="msg">Application error!</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-fix">
+                            <span class="time">12 hrs</span>
+                            <i class="fa fa-wrench"></i>
+                            <span class="msg">Installation Succeeded.</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="notification-success">
+                            <span class="time">18 hrs</span>
+                            <i class="fa fa-check"></i>
+                            <span class="msg">Account Created. </span>
+                        </a>
+                    </li>
+                </div>
+                <li class="dd-footer"><a href="#">View All Notifications</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#" id="headerbardropdown"><span><i class="fa fa-level-down"></i></span></a>
+        </li>
+        <li class="dropdown demodrop">
+            <a href="#" class="dropdown-toggle tooltips" data-toggle="dropdown"><i class="fa fa-magic"></i></a>
+
+            <ul class="dropdown-menu arrow dropdown-menu-form" id="demo-dropdown">
+                <li>
+                    <label for="demo-header-variations" class="control-label">Header Colors</label>
+                    <ul class="list-inline demo-color-variation" id="demo-header-variations">
+                        <li><a class="color-black" href="javascript:;"  data-headertheme="header-black.css"></a></li>
+                        <li><a class="color-dark" href="javascript:;"  data-headertheme="default.css"></a></li>
+                        <li><a class="color-red" href="javascript:;" data-headertheme="header-red.css"></a></li>
+                        <li><a class="color-blue" href="javascript:;" data-headertheme="header-blue.css"></a></li>
+                    </ul>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <label for="demo-color-variations" class="control-label">Sidebar Colors</label>
+                    <ul class="list-inline demo-color-variation" id="demo-color-variations">
+                        <li><a class="color-lite" href="javascript:;"  data-theme="default.css"></a></li>
+                        <li><a class="color-steel" href="javascript:;" data-theme="sidebar-steel.css"></a></li>
+                        <li><a class="color-lavender" href="javascript:;" data-theme="sidebar-lavender.css"></a></li>
+                        <li><a class="color-green" href="javascript:;" data-theme="sidebar-green.css"></a></li>
+                    </ul>
+                </li>
+                <li class="divider"></li>
+                <li>
+                    <label for="fixedheader">Fixed Header</label>
+                    <div id="fixedheader" style="margin-top:5px;"></div>
+                </li>
+            </ul>
+        </li>
+
 
     </ul>
 </header>
@@ -181,7 +326,9 @@ if (!session_id()) session_start();
             <li class="hasChild"><a href="javascript:;"><i class="fa fa-pencil"></i> <span>权限管理</span>
                     <span class="badge badge-primary">5</span></a>
                 <ul class="acc-menu">
+                    <li><a href="/orchard7/privilege/addRole">添加角色</a></li>
                     <li><a href="/orchard7/privilege/role">角色管理</a></li>
+                    <li><a href="/orchard7/privilege/credential">添加人员</a></li>
                     <li><a href="/orchard7/privilege/credential">人员管理</a></li>
                 </ul>
             </li>
