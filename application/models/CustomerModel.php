32a99971aa89849ad8a6a4905117e206b7628a45
hhhunteres <harry.ljian@hotmail.com>
2014-06-18 10:34:12 +0800

    add combo feature

--- application/models/CustomerModel.php
+++ application/models/CustomerModel.php
@@ -28,7 +28,7 @@ class CustomerModel
 
     public function getCustomerByCellphone($cellphone) {
         $sql = "SELECT * FROM customer WHERE cellphone='".$cellphone."'";
-        echo "Customer sql is ".$sql;
+//        echo "Customer sql is ".$sql;
         $query = $this->db->prepare($sql);
         $query->execute();
 
