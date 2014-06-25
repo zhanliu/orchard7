600a3e15d617658112ebb8bad706c3a0b3dbb9bd
hhhunteres <harry.ljian@hotmail.com>
2014-06-25 11:56:38 +0800

    compress file

--- application/views/common/upload.php
+++ application/views/common/upload.php
@@ -1,5 +1,7 @@
 <?php
 
+
+
 // A list of permitted file extensions
 $allowed = array('png', 'jpg', 'gif', 'jpeg');
 
@@ -17,6 +19,22 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 
 	if(move_uploaded_file($_FILES['upl']['tmp_name'], 'public/uploads/'.$file_name)){
 		echo '{"status":"success"}';
+        //////////////////
+        $filename = 'public/uploads/'.$file_name;
+        $percent = 0.5;
+
+        header('Content-Type: image/jpeg');
+
+        list($width, $height) = getimagesize($filename);
+        $new_width = $width * $percent;
+        $new_height = $height * $percent;
+
+        $image_p = imagecreatetruecolor($new_width, $new_height);
+        $image = imagecreatefromjpeg($filename);
+        imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+
+        imagejpeg($image_p, $filename, 100);
+        //////////////////
 		exit;
 	}
 }
