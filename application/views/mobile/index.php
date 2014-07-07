4b1b74b7511efb8574ad6176fd0e62cf7bec8f23
Jeru <jeru.liu@gmail.com>
2014-07-07 23:04:10 +0800

    Create mobile index

--- application/views/mobile/index.php
+++ application/views/mobile/index.php
@@ -11,10 +11,14 @@
                         <strong>注意!</strong> 当前配送范围仅限广州市越秀区
                     </div>
 
+                    <form action="<?php echo URL; ?>mobile/showcase" method="post" class="form-horizontal">
+                        <input type="hidden" name="province" value="广东省">
+                        <input type="hidden" name="city" value="广州市">
+                        <input type="hidden" name="district" value="越秀区">
                     <div id="form-bg">
                         <fieldset>
                             <div class="form-group">
-                                <label class="col-sm-3 control-label">广东省广州市越秀区</label>
+                                <label class="col-sm-3 control-label">广东省-广州市-越秀区</label>
                                 <div class="col-sm-6">
                                     <input type="text" id="address1" name="address1" size="30" required="required" class="form-control" placeholder="输入路名和小区...">
                                 </div>
@@ -29,11 +33,12 @@
 
                             <div class="stepy-navigator panel-footer"><div class="pull-right">
 
-                                    <a href="#" onclick="next2()" class="btn btn-primary">Next <i class="fa fa-long-arrow-right"></i></a>
+                                    <a href="#" onclick="next()" class="btn btn-primary">Next <i class="fa fa-long-arrow-right"></i></a>
                                 </div></div>
                         </fieldset>
 
                     </div>
+                    </form>
 
                 </div>
 
