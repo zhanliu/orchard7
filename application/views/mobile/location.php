896d6ca84e88f95f71afbf75c3d62437c15d0e6a
Jeru <jeru.liu@gmail.com>
2014-07-07 20:42:42 +0800

    Updated location UI

--- application/views/mobile/location.php
+++ application/views/mobile/location.php
@@ -1,55 +1,41 @@
 <div id="container">
     <div id="main" role="main">
         <div id="contentWrapper">
-            <div id="authView" class="view current" data-url="/auth/signup/?ref=auth">
-                <div id="header">
-                    <span class="auth_tagline">something special<br>every day</span>
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <h4>确认配送范围</h4>
                 </div>
 
-
-                <div id="signup" class="auth">
+                <div class="panel-body">
                     <div class="form-padding">
                         <div id="form-bg">
                             <form action="" method="post" id="form-signup" autocomplete="on">
 
-                                <input type="hidden" name="firstname" id="firstname" value=""/>
-                                <input type="hidden" name="lastname" id="lastname" value=""/>
-
                                 <fieldset>
                                     <div class="form-item">
-                                        <input required="required" placeholder="您要配送的街道或小区..." type="address" name="address" autocorrect="off" autocapitalize="off" id="email"  title="地址" class="input-text validate-email required-entry"/>
+                                        <input required="required" placeholder="您要配送的大楼或小区..." type="address" name="address" class="form-control"/>
                                     </div>
                                 </fieldset>
 
-                                <button type="submit" title="start shopping now" class="ok" id="shopNowButton">开始定位</button>
-
-
+                                <button type="submit" title="start shopping now" class="ok" id="shopNowButton">开始查找</button>
                             </form>
+                        </div>
+                    </div>
+
+                    <div id="why-join">
+                        <div class="centerbox">
+                            <h3>请输入关键字让我们知道您是否在配送范围内，例如:</h3>
+                            <ul>
+                                <li>海印广场</li>
+                                <li>又一城</li>
+                                <li>TIT创意园</li>
+                            </ul>
+                            <h3>说明: 当前分店仅分布于广州市海印区</h3>
                         </div>
                     </div>
                 </div>
 
-                <div id="why-join">
-                    <div class="centerbox">
-                        <h2>水果的妙用</h2>
-                        <ul>
-                            <li>养生之道</li>
-                            <li>变废为宝，奇妙的果皮</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="sliderContainer visibleNearby fullWidth clearfix">
-                    <div class="centerbox"><h2>今日</h2></div>
-                    <div id="gallery-1" class="royalSlider rsDefault">
-                        <a class="rsImg"  data-rsDelay="1000"  href="https://zcdn.global.ssl.fastly.net/images/cache/event/200x1000/89762_merrellwomen_HP_2014_0702_SJO1_2_1403829409.jpg"><span class="description">apparel & footwear</span></a>
-                    </div>
-                </div>
-
-
             </div>
-
-
         </div>
     </div>
 </div>
