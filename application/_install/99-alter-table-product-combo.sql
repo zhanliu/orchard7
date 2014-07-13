051650189d6369fc24d52576d590b501c2e43916
hhhunteres <harry.ljian@hotmail.com>
2014-07-13 14:51:35 +0800

    add all

--- application/_install/99-alter-table-product-combo.sql
+++ application/_install/99-alter-table-product-combo.sql
@@ -20,10 +20,10 @@ Modify column combo_quantity int(11) NULL;
 
 /* ------------ */
 
-alter table `order_details` delete column product_id;
-alter table `order_details` delete column product_quantity;
-alter table `order_details` delete column combo_id;
-alter table `order_details` delete column combo_quantity;
+alter table `order_details` drop product_id;
+alter table `order_details` drop product_quantity;
+alter table `order_details` drop combo_id;
+alter table `order_details` drop combo_quantity;
 
 alter table `order_details` add column item_id int(20) after `order_id`;
 alter table `order_details` add column item_quantity int(11) after `item_id`;
