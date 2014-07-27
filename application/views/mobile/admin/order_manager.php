7d3eda1e0c49495763ffe74bc36a716f7583dd0c
Jeru <jeru.liu@gmail.com>
2014-07-27 22:17:59 +0800

    Refined process order UI

--- application/views/mobile/admin/order_manager.php
+++ application/views/mobile/admin/order_manager.php
@@ -22,54 +22,55 @@
 
                     <div id="page-inside">
 
-                        <div class="full-content">
+                        <div class="alert alert-danger">
+                            今日订单 <br/>
+                            <a href="<?php echo URL; ?>mobileadmin/orderManager/0"
+                               class="myButton">未处理(<?php echo $amount_of_type_0; ?>)</a>
+                            <a href="<?php echo URL; ?>mobileadmin/orderManager/1"
+                               class="myButton">已处理(<?php echo $amount_of_type_1; ?>)</a>
+                            <a href="<?php echo URL; ?>mobileadmin/orderManager/9"
+                               class="myButton">全部(<?php echo $amount_of_type_9; ?>)</a>
+                        </div>
 
-                            <div class="alert alert-danger">
-                                今日订单 <br />
+                        <?php
+                        foreach ($orders as $order) {
+                            $id = $order->id;
+                            $full_address = $order->province . $order->city . $order->district . $order->address1 . $order->address2;
+                            //$qty = $items[$product->id];
+                            $order_reliable = $order->is_verified ? "老客订单" : "新客订单";
+                            $order_reliable_bg = $order->is_verified ? "#008" : "#800";
+                            ?>
 
-                                <a href="<?php echo URL; ?>mobileadmin/orderManager/0" class="myButton" >未处理(<?php echo $amount_of_type_0; ?>)</a>
-                                <a href="<?php echo URL; ?>mobileadmin/orderManager/1" class="myButton" >已处理(<?php echo $amount_of_type_1; ?>)</a>
-                                <a href="<?php echo URL; ?>mobileadmin/orderManager/9" class="myButton" >全部(<?php echo $amount_of_type_9; ?>)</a>
-                            </div>
+                            <div id="list_<?php echo $id; ?>">
 
-                                <?php
-                                foreach ($orders as $order) {
-                                    $id = $order->id;
-                                    $full_address = $order->province.$order->city.$order->district.$order->address1.$order->address2;
-                                    //$qty = $items[$product->id];
-                                    $order_reliable = $order->is_verified ? "老客订单" : "新客订单";
-                                    $order_reliable_bg = $order->is_verified ? "#008" : "#800";
-                                    ?>
+                                <div class="name" style="font-size:18px;padding:3px">订单号:
+                                    <a href="<?php echo URL; ?>mobileadmin/processOrder/<?php echo $order->id; ?>"
+                                       class="myButton">
+                                        <?php echo $order->order_number; ?>
+                                    </a>
+                                    (<?php echo $order->status; ?>)
+                                </div>
+                                <div class="desc" style="color:#000;padding:5px">
 
-                                    <div id="list_<?php echo $id; ?>">
-
-                                        <div class="name" style="font-size:18px;padding:3px">订单号:
-                                            <a href="<?php echo URL; ?>mobileadmin/processOrder/<?php echo $order->id; ?>" class="myButton" >
-                                                <?php echo $order->order_number; ?>
-                                            </a>
-                                            (<?php echo $order->status; ?>)
-                                        </div>
-                                        <div class="desc" style="color:#000;padding:5px">
-
-                                            <span style="color:#000;padding:3px">配送店铺: <?php echo $order->storename; ?> </span><br>
-                                            <span style="color:#000;padding:3px">下单时间: <?php echo $order->created_time; ?>, </span><br>
-                                            <span style="color:#000;padding:3px">总价: <?php echo $order->total_amount; ?>, </span>
-                                            <span style="color:#000;padding:3px">运费: <?php echo $order->delivery_fee; ?>, </span>
-                                            <span style="background:<?php echo $order_reliable_bg?>;color:#fff">
+                                    <span
+                                        style="color:#000;padding:3px">配送店铺: <?php echo $order->storename; ?> </span><br>
+                                    <span style="color:#000;padding:3px">下单时间: <?php echo $order->created_time; ?>
+                                         </span><br>
+                                    <span style="color:#000;padding:3px">总价: <?php echo $order->total_amount; ?>
+                                        , </span>
+                                    <span style="color:#000;padding:3px">运费: <?php echo $order->delivery_fee; ?>
+                                        , </span>
+                                            <span style="background:<?php echo $order_reliable_bg ?>;color:#fff">
                                                 <?php echo $order_reliable; ?>
                                             </span>
-                                            <span style="color:#000;padding:3px">地址: <?php echo $full_address; ?></span>
+                                    <span style="color:#000;padding:3px">地址: <?php echo $full_address; ?></span>
 
-                                        </div>
-                                     </div>
+                                </div>
+                            </div>
 
-                                    <hr>
-                                <?php } ?>
+                            <hr>
+                        <?php } ?>
 
-
-
-
-                        </div>
 
                     </div>
                 </div>
--- application/views/mobile/admin/process_order.php
+++ application/views/mobile/admin/process_order.php
@@ -20,52 +20,66 @@
                     </header>
                     <div class="clr"></div>
 
-
-                    <div style="color:#f00;padding:5px">
+                    <div id="page-inside">
 
                         <?php
                         $order = $order[0];
                         $full_address = $order->province . $order->city . $order->district . $order->address1 . $order->address2;
+                        $order_reliable = $order->is_verified ? "老客订单" : "新客订单";
+                        $order_reliable_bg = $order->is_verified ? "#008" : "#800";
                         ?>
-                        <div class="alert alert-danger name">
+                        <h1>
                             订单号:<?php echo $order->order_number; ?>
-                        </div>
+                        </h1>
 
+                        <div style="color:#555;padding:5px">
+                            <h3>基本信息</h3>
 
-                        <h3>基本信息</h3>
+                            <div>配送店铺: <?php echo $order->storename; ?></div>
+                            <div>下单时间: <?php echo $order->created_time; ?></div>
+                            <div>总价: <?php echo $order->total_amount; ?> | 运费: <?php echo $order->delivery_fee; ?> | 
+                                <span style="background:<?php echo $order_reliable_bg ?>;color:#fff">
+                                    <?php echo $order_reliable; ?>
+                                </span>
+                            </div>
 
-                        <div>配送店铺: <?php echo $order->storename; ?></div>
-                        <div>下单时间: <?php echo $order->created_time; ?></div>
-                        <div>总价: <?php echo $order->total_amount; ?> | 运费: <?php echo $order->delivery_fee; ?></div>
-                        <div>订单状态: <?php echo $order->status; ?>
+                            <hr>
 
-                            <?php if ($order->status_code == 0) { ?>
-                                <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/1" class="myButton" >待派送</a>
-                                <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/5" class="myButton" >取消订单</a>
-                            <?php } else if ($order->status_code == 1) { ?>
-                                <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/2" class="myButton" >派送中</a>
-                            <?php } else if ($order->status_code == 2) { ?>
-                                <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/3" class="myButton" >完成</a>
-                                <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/4" class="myButton" >拒收</a>
+                            <h3>客户信息</h3>
+
+                            <div>姓名: <?php echo $order->cname; ?></div>
+                            <div>电话: <?php echo $order->cellphone; ?></div>
+                            <div>地址: <?php echo $full_address; ?></div>
+
+                            <hr>
+
+                            <h3>订单详情</h3>
+                            <?php foreach ($items as $item) { ?>
+                                <div>商品名: <?php echo $item->name; ?></div>
+                                <div>价格: <?php echo $item->price; ?> | 数量: <?php echo $item->item_quantity; ?></div><br>
                             <?php } ?>
 
+                            <h3>状态Update</h3>
+
+                            <div>订单状态: <?php echo $order->status; ?>
+
+                                <?php if ($order->status_code == 0) { ?>
+                                    <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/1"
+                                       class="myButton">待派送</a>
+                                    <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/5"
+                                       class="myButton">取消订单</a>
+                                <?php } else if ($order->status_code == 1) { ?>
+                                    <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/2"
+                                       class="myButton">派送中</a>
+                                <?php } else if ($order->status_code == 2) { ?>
+                                    <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/3"
+                                       class="myButton">完成</a>
+                                    <a href="<?php echo URL; ?>mobileadmin/updateOrder/<?php echo $order->id; ?>/4"
+                                       class="myButton">拒收</a>
+                                <?php } ?>
+
+                            </div>
                         </div>
-
-                        <hr>
-
-                        <h3>客户信息</h3>
-
-                        <div>姓名: <?php echo $order->cname; ?></div>
-                        <div>电话: <?php echo $order->cellphone; ?></div>
-                        <div>地址: <?php echo $full_address; ?></div>
-
-                        <hr>
-
-                        <h3>订单详情</h3>
-                        <?php foreach ($items as $item) { ?>
-                            <div>商品名: <?php echo $item->name; ?></div>
-                            <div>价格: <?php echo $item->price; ?> | 数量: <?php echo $item->item_quantity; ?></div><br>
-                        <?php } ?>
 
                     </div>
 
