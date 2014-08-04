f565a8b4810b505bfb6bbcfc12fa540a0b318368
harry.li <harry.ljian@hotmail.com>
2014-08-04 23:44:04 +0800

    add all

--- application/views/common/upload.php
+++ application/views/common/upload.php
@@ -5,6 +5,8 @@ $allowed = array('JPG', 'jpg', 'jpeg');
 
 if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 
+    $prefix_name = $_POST["upload_inner_img_name_prefix"];
+
     if (isset($_POST['fixed_upload_img_name'])) {
         $file_name = $_POST['fixed_upload_img_name'];
     } else {
@@ -15,22 +17,23 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
             exit;
         }
 
-        $prefix_name = $_POST["upload_inner_img_name_prefix"];
         $file_name = $prefix_name."_".$_FILES['upl']['name'];
     }
 
+
 	if (ONLINE == 'FALSE') { // offline
-	
+
+        // save 100*100 product image
 		if(move_uploaded_file($_FILES['upl']['tmp_name'], 'public/uploads/'.$file_name)){
 
 			echo '{"status":"success"}';
 
 			$filename = 'public/uploads/'.$file_name;
-			$percent = 0.5;////////////// file compress
+			//$percent = 2/3;////////////// file compress
 			header('Content-Type: image/jpeg');
 			list($width, $height) = getimagesize($filename);
-			$new_width = $width * $percent;
-			$new_height = $height * $percent;
+			$new_width = $width * 2 / 3;
+			$new_height = $height * 2 / 3;
 			$image_p = imagecreatetruecolor($new_width, $new_height);
 			$image = imagecreatefromjpeg($filename);
 			imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
@@ -40,6 +43,28 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 		} else{
 			echo "There was an error uploading the file, please try again!";
 		}
+
+        // save 150*150 product image
+        $file_name = 'b_' .$file_name;
+        if(move_uploaded_file($_FILES['upl']['tmp_name'], 'public/uploads/'.$file_name)){
+
+            echo '{"status":"success"}';
+
+            $filename = 'public/uploads/'.$file_name;
+            //$percent = 2/3;////////////// file compress
+            header('Content-Type: image/jpeg');
+            list($width, $height) = getimagesize($filename);
+            $new_width = $width;
+            $new_height = $height;
+            $image_p = imagecreatetruecolor($new_width, $new_height);
+            $image = imagecreatefromjpeg($filename);
+            imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+            imagejpeg($image_p, $filename, 100);
+
+            exit;
+        } else{
+            echo "There was an error uploading the file, please try again!";
+        }
 		
 	} else { // online
 		$target_path = SAE_TMP_PATH;
@@ -47,12 +72,14 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 		$domain='product';
 		//$uuid=md5(uniqid(rand(), true));
 		$target_path = $target_path.$prefix_name;
+
+        // save 100*100 image
 		if(move_uploaded_file($_FILES['upl']['tmp_name'], $target_path)) {
-			$percent = 0.5;////////////// file compress
+			//$percent = 2/3;////////////// file compress
 			header('Content-Type: image/jpeg');
 			list($width, $height) = getimagesize($target_path);
-			$new_width = $width * $percent;
-			$new_height = $height * $percent;
+			$new_width = $width * 2 / 3;
+			$new_height = $height * 2 / 3;
 			$image_p = imagecreatetruecolor($new_width, $new_height);
 			$image = imagecreatefromjpeg($target_path);
 			imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
@@ -69,7 +96,33 @@ if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 		} else{
 			echo "There was an error uploading the file, please try again!";
 		}
-		
+
+        // save 150 * 150 image
+        $prefix_name = 'b_' . $prefix_name;
+        $target_path = SAE_TMP_PATH.$prefix_name;
+        $file_name = 'b_' . $file_name;
+        if(move_uploaded_file($_FILES['upl']['tmp_name'], $target_path)) {
+            //$percent = 2/3;////////////// file compress
+            header('Content-Type: image/jpeg');
+            list($width, $height) = getimagesize($target_path);
+            $new_width = $width;
+            $new_height = $height;
+            $image_p = imagecreatetruecolor($new_width, $new_height);
+            $image = imagecreatefromjpeg($target_path);
+            imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+            imagejpeg($image_p, $target_path, 100);
+
+
+            $file_contents= file_get_contents($target_path);
+            $s = new SaeStorage();
+            $filename = $file_name;
+            $s->write($domain, $filename ,$file_contents);
+            $url=$s->getUrl($domain, $filename );
+
+            exit;
+        } else{
+            echo "There was an error uploading the file, please try again!";
+        }
 	}
 }
 
